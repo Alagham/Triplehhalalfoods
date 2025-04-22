fe1533ce5aa1033918702d68607a0fde38672e72
Alagham <hamzahalagbe27@gmail.com>
2025-04-22 21:20:37 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Triple H' Halal foods</h1>
+      <h5>Page under maintainance...............</h5>
+    </div>
+  )
+}
+
+export default App
